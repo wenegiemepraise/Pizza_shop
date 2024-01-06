c43fbd979cf162bea100e16c942489b3a6fa2118
Praise Wenegieme <126319573+wenegiemepraise@users.noreply.github.com>
2024-01-06 20:22:00 +0000

    Update IS1110.py

--- IS1110.py
+++ IS1110.py
@@ -1,7 +1,14 @@
+import random
+
 print("Welcome to the pizza shop situated in Cork Airport. It's a pleasure to have you :) ")
 
+fname = ""
+sname = ""
+p_number = ""
+email = ""
 
-def first_name():
+
+def get_first_name():
     while True:
         global fname
         fname = input("First off, may I have your first name please: ").capitalize()
@@ -17,10 +24,7 @@ def first_name():
             return "Great!"
 
 
-print(first_name())
-
-
-def surname():
+def get_surname():
     while True:
         global sname
         sname = input(f"\nNice to meet you {fname} and what is your surname: ").capitalize()
@@ -36,21 +40,16 @@ def surname():
             return "Great!"
 
 
-print(surname())
-
-
-def phone_number():
+def get_phone_number():
     while True:
         global p_number
         p_number = input("\nCan you enter your phone number please: ")
         # Making sure the phone number is the required length
-        if len(p_number) != 9 or len(p_number) == 0:
-            print(f"Sorry your number is either too long or too short.\n Try again.")
-            print("Remember not to use 0 at the start of your number.")
+        if len(p_number) != 10 and p_number != p_number.isdigit():
+            print(f"Sorry please enter a 10 digit number\n Try again.")
             continue
-        # Checking if they put in an input
         choice3 = input(
-            f"Is ({p_number[0:2]}) {p_number[2:-1]} the correct phone number? "
+            f"Is +353 ({p_number[1:3]}) {p_number[3:]} the correct phone number? "
             f"If it is not press N but if it is press any other key: ")
         if choice3.upper() == "N":
             print("\n Ok lets take a step back")
@@ -59,10 +58,7 @@ def phone_number():
             return "Perfect!"
 
 
-print(phone_number())
-
-
-def get_email(): 
+def get_email():
     while True:
         global email
         email = input("\nFinally what is your email: ")
@@ -71,7 +67,7 @@ def get_email():
         # Link here:
         # https://www.w3schools.com/python/ref_list_count.asp
         validation = email.count("@")
-        if validation != 1 or len(email) == 0:
+        if validation != 1 or len(email) < 4:
             print("Invalid email :( \n Make sure to add an  @ to the email")
             continue
         choice4 = input(f"Is {email} the correct email? If not press N but if it is press any other key: ")
@@ -82,11 +78,75 @@ def get_email():
             return "\nWe are almost there."
 
 
+def pizza_cost(def_pizza, def_pizza_size):
+    # Define the prices for each type of pizza
+    prices = {
+        "margherita": {"small": 10, "medium": 12, "large": 15},
+        "pepperoni": {"small": 12, "medium": 14, "large": 17},
+        "four seasons": {"small": 11.50, "medium": 13.50, "large": 16.50}
+    }
+
+    pcost = 0
+    # Iterate through the selected pizzas
+    for j in range(len(def_pizza)):
+        selected_pizza = def_pizza[j]
+        selected_pizza_size = def_pizza_size[j]
+
+        pcost += prices[selected_pizza][selected_pizza_size]
+
+    return pcost
+
+
+def dips_toppings_cost(def_dip, def_topping):
+    topping_prices = {"rucola": 1.50, "mushrooms": 2, "mushroom": 2}
+    dip_prices = {"garlic": 3, "spicy mayo": 3}
+
+    tcost = 0
+    # Iterate through the selected pizzas
+    for def_topping in topping_order:
+        tcost += topping_prices[def_topping]
+
+    for def_dip in dip_order:
+        tcost += dip_prices[def_dip]
+
+    return tcost
+
+
+def drinks_sum(selected_drinks, selected_drink_sizes):
+    # Define the prices for each type of drink
+    prices_drinks = {
+        "coke": {"small": 2, "medium": 2.50, "large": 3.50},
+        "pepsi": {"small": 2, "medium": 2, "large": 3.50},
+        "fanta": {"small": 2, "medium": 2.50, "large": 3.50},
+        "7up": {"small": 2, "medium": 2.50, "large": 3.50},
+        "water": {"small": 1, "medium": 2, "large": 2.50}
+    }
+
+    dcost = 0
+    # Iterate through the indices of the lists
+    for k in range(len(selected_drinks)):
+        def_drinks = selected_drinks[k]
+        def_drink_sizes = selected_drink_sizes[k]
+        dcost += prices_drinks[def_drinks][def_drink_sizes]
+
+    return dcost
+
+
+def generate_receipt_number():
+    # Generate a unique receipt number using a random number
+    random_number = random.randint(1000, 9999)
+    receipt_number = f"- {random_number}"
+    return receipt_number
+
+
+print(get_first_name())
+print(get_surname())
+print(get_phone_number())
 print(get_email())
 
 print(
-    f"\nGreat! Nice to meet you {fname} {sname}. \n Your phone no. is: +353 ({p_number[0:2]}) {p_number[2:-1]}"
-    f" and email is: {email}")
+    f"\nGreat! Nice to meet you {fname} {sname}. \n Your phone no. is: +353 ({p_number[1:3]}) {p_number[3:]}"
+    f" and your email is {email}")
 
 print("\nTake a look at our wide selection of Pizzas")
 
@@ -141,6 +201,7 @@ while True:
         if order_confirmation.upper() == "N":
             pizza = []
             pizza_size = []
+            print("Lets restart your order!")
 
         else:
             print("Perfect!")
@@ -150,28 +211,7 @@ while True:
         print("\n Sorry, please enter a number not a word. :)")
 
 
-def pizza_cost(pizza, pizza_size):
-    # Define the prices for each type of pizza
-    prices = {
-        "margherita": {"small": 10, "medium": 12, "large": 15},
-        "pepperoni": {"small": 12, "medium": 14, "large": 17},
-        "four seasons": {"small": 11.50, "medium": 13.50, "large": 16.50}
-    }
-
-    pcost = 0
-    # Iterate through the selected pizzas
-    for i in range(len(pizza)):
-        p = pizza[i]
-        s = pizza_size[i]
-
-        # Check if the pizza type and size are valid
-        if p in prices and s in prices[p]:
-            pcost += prices[p][s]
-
-    return pcost
-
-
-print(f"your total cost for the pizzas is: €{pizza_cost(pizza=pizza, pizza_size=pizza_size)}")
+print(f"Your total cost for the pizzas is: €{pizza_cost(def_pizza=pizza, def_pizza_size=pizza_size)}")
 
 topping_order = []  # Initialize an empty list for toppings
 dip_order = []  # Initialize an empty list for dips
@@ -188,10 +228,16 @@ while want_extras:
         print("Take a look at our choice of toppings and dips")
         print("\nHere are our dips and toppings:")
         print("_____________________________________")
-        print("Toppings/Dips            € per pizza ")
+        print("Toppings                 € per pizza ")
         print("-------------------------------------")
         print("Rucola                       1.50    ")
         print("Mushrooms                     2      ")
+        print("_____________________________________")
+
+
+        print("_____________________________________")
+        print("Dips                      € per pizza")
+        print("-------------------------------------")
         print("Garlic                        3      ")
         print("Spicy mayo                    3      ")
         print("_____________________________________")
@@ -222,12 +268,14 @@ while want_extras:
             # Check if they have entered the right amount of topping if they have not reset topping
             if len(topping_order) != choice_toppings or len(dip_order) != choice_dips:
                 print("\n You have made an error in your inputs, so we will restart")
+                topping_order = []
+                dip_order = []
                 want_extras = True
 
             print(f"The toppings you have ordered is {topping_order} and dips you have ordered is {dip_order}")
             restart_extras = str(input(
-                "\n Do you want to restart your order on toppings and dip.\nIf you want to enter, "
-                "please enter Y or else enter any key: "))
+                "\n Do you want to restart your order on toppings and dip.\nIf you do "
+                "please enter Y or else enter any key to continue: "))
 
             if restart_extras == "Y":
                 print("\n Ok lets take a step back")
@@ -237,29 +285,14 @@ while want_extras:
 
         except ValueError:
             print("\n Please use a number for the amount of toppings or dips you want.")
-
-
-def dips_toppings_cost(dip, topping):
-    topping_prices = {"rucola": 1.50, "mushrooms": 2, "mushroom": 2}
-    dip_prices = {"garlic": 3, "spicy mayo": 3}
-
-    tcost = 0
-    # Iterate through the selected pizzas
-    for topping in topping_order:
-        if topping in topping_prices:
-            tcost += topping_prices[topping]
-
-    for dip in dip_order:
-        if dip in dip_prices:
-            tcost += dip_prices[dip]
-
-    return tcost
+            want_extras = True
 
 
 print(
-    f"Your total cost for dips and toppings is: €{dips_toppings_cost(dip=dip_order, topping=topping_order)}")
+    f"Your total cost for dips and toppings is: €{dips_toppings_cost(def_dip=dip_order, def_topping=topping_order)}")
 
-drinks = {}
+drinks = []
+drink_sizes = []
 want_drinks = True
 while want_drinks:
     want_drinks = False
@@ -287,82 +320,66 @@ while want_drinks:
 
             if amount_drinks < 1:
                 print("\n Please enter a valid amount of drinks.")
-
-            if amount_drinks < 1:
-                print("\n Please enter a valid amount of drinks.")
+                want_drinks = True
+                continue
 
             for i in range(amount_drinks):
-                choice_drinks = str(input("\n What drink would you like: "))
+                choice_drinks = str(input("\n What drink would you like: ")).lower()
                 if choice_drinks.lower() not in ["coke", "pepsi", "fanta", "7up", "water"]:
                     print("You have made an invalid choice please try again")
                 else:
-                    choice_size_drinks = str(input("\n What size of drink would you like: "))
-                    if choice_size_drinks.lower() not in ["small", "medium", "large"]:
-                        print("\n You have made an invalid choice please try again")
-                    else:
-                        drinks[choice_drinks] = choice_size_drinks
-                        print("\n Drink added!")
+                    drinks.append(choice_drinks)
+                choice_size_drinks = str(input("\n What size of drink would you like: ")).lower()
+                if choice_size_drinks.lower() not in ["small", "medium", "large"]:
+                    print("\n You have made an invalid choice please try again")
+                else:
+                    drink_sizes.append(choice_size_drinks)
+                    print("\n Drink added!")
 
-            print(f"The drinks you have chosen are {drinks}.")
+            print(f"The drinks you have chosen are {drinks} with the corresponding sizes {drink_sizes}.")
             restart_drinks = str(input("Do you want to restart your drink selection. Yes or No: ")).lower()
             if restart_drinks == "yes":
-                drinks = {}
+                drinks = []
+                drink_sizes = []
                 want_drinks = True
+                print("Ok let's take a step back.")
             elif restart_drinks == "no":
                 print("Ok lets continue!")
-
+            if len(drinks) != amount_drinks or len(drink_sizes) != amount_drinks:
+                print("You have seen to have made an error please try again.")
+                want_drinks = True
         except ValueError:
             print("\n Sorry you need to use a number to indicate how many drinks you want.")
             want_drinks = True
 
 
-def drinks_sum(drinks_dict):
-    # Define the prices for each type of drink
-    prices_drinks = {
-        "coke": {"small": 2, "medium": 2.50, "large": 3.50},
-        "pepsi": {"small": 2, "medium": 2, "large": 3.50},
-        "fanta": {"small": 2, "medium": 2.50, "large": 3.50},
-        "7up": {"small": 2, "medium": 2.50, "large": 3.50},
-        "water": {"small": 1, "medium": 2, "large": 2.50}
-    }
-
-    dcost = 0
-    # Iterate through the selected drinks and sizes in the dictionary
-    for drink, size in drinks_dict.items():
-        if drink in prices_drinks and size in prices_drinks[drink]:
-            dcost += prices_drinks[drink][size]
-
-    return dcost
-
-
-print(f"\n Your total cost for drinks is: €{drinks_sum(drinks_dict=drinks)}")
+print(f"\n Your total cost for drinks is: €{drinks_sum(selected_drinks=drinks, selected_drink_sizes=drink_sizes)}")
 
 # Add a delivery service and the further out they are the higher the cost
 while True:
     delivery_cost = 0
     try:
-        delivery_in_cork = str(input("\n Do you live inside Cork yes or no: "))
+        delivery_in_cork = str(input("\n Do you live inside Cork yes or no: ")).lower()
         if delivery_in_cork.lower() == "yes":
-            towns_in_cork = ()
             delivery = int(input("\n Great how many minutes away from the airport are you: "))
             if delivery <= 10:
                 delivery_cost = 0
                 break
             elif 10 < delivery <= 20:
-                delivery_cost = 10
+                delivery_cost = 5
                 break
             elif 20 < delivery <= 30:
-                delivery_cost = 15
+                delivery_cost = 10
                 break
             elif 30 < delivery < 60:
-                delivery_cost = 30
+                delivery_cost = 20
                 break
             else:
-                delivery_cost = 45
+                delivery_cost = 30
                 break
 
         elif delivery_in_cork.lower() == "no":
-            delivery_cost = 50
+            delivery_cost = 40
             break
 
     except ValueError:
@@ -370,25 +387,57 @@ while True:
         continue
 
 print(f"\n The cost of your delivery is: €{delivery_cost}")
-
-cost_before_vat = (delivery_cost + drinks_sum(drinks_dict=drinks) + dips_toppings_cost(dip=dip_order,
-                                                                                       topping=topping_order) + pizza_cost(
-    pizza=pizza, pizza_size=pizza_size))
-if cost_before_vat >= 35:
-    discount = cost_before_vat / 10
-    cost_before_vat -= discount
+end_cost = (
+        delivery_cost + drinks_sum(selected_drinks=drinks, selected_drink_sizes=drink_sizes) + dips_toppings_cost(
+            def_dip=dip_order,
+            def_topping=topping_order) + pizza_cost(
+            def_pizza=pizza, def_pizza_size=pizza_size))
+discount = 0
+if end_cost >= 35:
+    discount = end_cost / 10
+    end_cost -= discount
     print(f"\n Due to your price being high you have received a discount of {discount}.")
-    print(f"\n Now your total price before vat is {cost_before_vat}")
-elif cost_before_vat >= 50:
-    discount = cost_before_vat / 15
-    cost_before_vat -= discount
+    print(f"\n Now your total price is {end_cost}")
+    # If the cost is really high give additional discounts
+elif end_cost >= 50:
+    discount = end_cost / 15
+    end_cost -= discount
     print(f"\n Due to your price being high you have received a discount of €{discount}.")
-    print(f"\n Now your total price before vat is €{cost_before_vat}")
+    print(f"\n Now your total price is €{end_cost}")
 
-cost_with_vat = cost_before_vat * 1.23
-print(f"\n Your cost with vat is €{round(cost_with_vat, 2)}")
-print("Thanks for ordering at our pizza shop. We hope you enjoy your food :D")
+vat = round(end_cost * .23, 2)
+print(f"\n Your vat cost is €{vat}")
+amount_tip = 0
+# Extra functionality asking if they want to tip
+tip = str(input("Would you like to add a tip to show appreciation for our staff? Y/N: ")).upper()
+if tip == "Y":
+    amount_tip = input("Would you like to add a 5 or 10 euro tip? ")
+    if amount_tip in ["five", "5"]:
+        tip = 5
+        end_cost += tip
+    elif amount_tip in ["ten", "10"]:
+        tip = 10
+        end_cost += tip
+else:
+    print("That's fine maybe next time")
 
-# Which is a better mechanic press any key or force them to press Y to continue and handle errors if they don't
-# Currency converter from euro to pounds or dollars
-# Create a GB settings at the start and if they choose gb then change phone number and also currency
+print(f"Your final cost is {end_cost}")
+
+
+f = open("receipt.txt", "w")
+f.write(f"{fname} {sname}")
+f.write("\nContact Info:")
+f.write(f"\nPhone number: +353 ({p_number[1:3]}) {p_number[3:]}")
+f.write(f"\nEmail: {email}")
+f.write(f"\nPizza cost: {pizza_cost(def_pizza=pizza, def_pizza_size=pizza_size)}")
+f.write(f"\nTopping and extras cost: {dips_toppings_cost(def_dip=dip_order, def_topping=topping_order)}")
+f.write(f"\nDrink cost: {drinks_sum(selected_drinks=drinks, selected_drink_sizes=drink_sizes)}")
+f.write(f"\nDeliver cost: {delivery_cost}")
+f.write(f"\nDiscount: {discount}")
+f.write(f"\nVat: {vat}")
+f.write(f"\nTip: {amount_tip}")
+f.write(f"\nTotal cost: {end_cost}")
+f.write(f"\nReceipt No: {generate_receipt_number()} ")
+f.close()
+
+print(f"\nThank you {fname} for ordering at our pizza shop. We hope you enjoy your food :D")
